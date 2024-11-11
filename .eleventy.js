2c53a179ca98e9fa41614bdbd4ef92123798538f
Kadek Satriadi <ananta.satriadi@gmail.com>
2024-11-11 19:50:10 +1100

    initial multi-language

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,42 @@
-const markdownIt = require('markdown-it');
-const markdownItAttrs = require('markdown-it-attrs');
+const { EleventyI18nPlugin } = require("@11ty/eleventy");
+
+const markdownIt = require("markdown-it");
+const markdownItAttrs = require("markdown-it-attrs");
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("src/assets");
-	eleventyConfig.pathPrefix = "/";
+	eleventyConfig.addWatchTarget("./_data/");
 
-	let markdownLibrary = markdownIt({
-		html: true
-	  }).use(markdownItAttrs);
-	  
-	  eleventyConfig.setLibrary("md", markdownLibrary);
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.pathPrefix = "/";
 
-    return {
-		dir: {
-			input: "src",
-			output: "public",
+  let markdownLibrary = markdownIt({
+    html: true,
+  }).use(markdownItAttrs);
+
+  eleventyConfig.setLibrary("md", markdownLibrary);
+
+  eleventyConfig.addPlugin(EleventyI18nPlugin, {
+    defaultLanguage: "en", // Required, this site uses "en"
+		// Rename the default universal filter names
+		filters: {
+			// transform a URL with the current page’s locale code
+			url: "locale_url",
+
+			// find the other localized content for a specific input file
+			links: "locale_links",
 		},
-	};
+
+		// When to throw errors for missing localized content files
+		errorMode: "strict", // throw an error if content is missing at /en/slug
+		// errorMode: "allow-fallback", // only throw an error when the content is missing at both /en/slug and /slug
+		// errorMode: "never", // don’t throw errors for missing content
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+	  data: "_data"
+    },
+  };
 };
---
+++ src/_data/i18n.js
@@ -0,0 +1,89 @@
+module.exports = {
+    en: {
+        nav:{
+            switchto: 'Switch content to ',
+            backtolanding: 'Back to homepage',
+            langenglish: 'English',
+            langindo: 'Indonesian'
+        },
+        hero: {
+            vision: "Empower Balinese communities in the preservation of Balinese culture through responsible, sustainable, innovative, and collaborative digitisation efforts."
+        },
+        about:{
+            title: "About", 
+            maintext: "Established in 2024, BADHI is a joint effort among Australian and Indonesian academics who believe that current and future digital technology offers opportunities to improve the preservation and utilisation of Bali's cultural heritage for social good."
+        },
+        projects:{
+            title: "Heritages",
+            maintext: "BADHI's projects aim to create lasting value for the Balinese people through cultural preservation. Our work spans both tangible and intangible heritage, with current projects focused on temple digital archival and lontar manuscript conservation.",
+            temples:{
+                title:"Temples",
+                maintext: 'For centuries, Bali"s ancient temples have faced destruction and rebuilding processes. We are creating sustainable solutions to preserve Balinese temples for generations to come. '
+            },
+            lontar:{
+                title:"Lontar",
+                maintext: 'Ancient Balinese palm leaf manuscripts hold centuries of sacred texts, history, and cultural knowledge. Their preservation is vital to protect this irreplaceable heritage for future generations. We explore responsible machine learning techniques to safeguard and interpret the knowledge contained in traditional Balinese palm leaf manuscripts.',
+               
+            }
+        },
+        team: {
+            maintext: 'BADHI is mainly led by the collaboration between the <a href="https://www.monash.edu/it/hcc/embodied-visualisation" target="_blank">Embodied Visualisation Research Group</a> (<b>EmVis</b>) at Monash University and the <a href="https://research.undiksha.ac.id/vvip-rg/" target="_blank">Virtual Vision, Image, and Pattern Research Group</a> (<b>VVIP</b>) at the Universitas Pendidikan Ganesha. In the Participatory Digital Archiving of Balinese Temple project, we collaborate with recordkeeping experts at the <a href="https://www.monash.edu/it/hcc/digital-transformation" target="_blank">Digital Transformation Research Group </a>(Monash University) and action research experts at the <a href="https://www.monash.edu/it/hcc/action-lab" target="_blank"> Action Lab </a>(Monash University). ',
+            title: 'Team',
+            academicstext: 'Collaborating academics:',
+            ratext: 'Research assistants', 
+            studenttext: 'Research students',
+            supportedby: 'Our efforts are also supported by:',
+            specialthanks: 'Special thanks to Ketut Wiryadana for creating our logo.'
+        },
+        contact:{
+            title: "Contact Us",
+            follow: "Follow our updates on",
+            link: "Several related links:",
+            maintext: 'If you are interested in supporting or joining our effort, please reach out to us at badhi.program[at]gmail.com',
+            phdtext: 'If you are interested in <b>PhD</b> around this topic, let use know by sending an expression of interest <a href="https://supervisorconnect.it.monash.edu/projects/research/visualisation-technology-cultural-heritage-preservation" target="_blank"> here</a>.'
+        }
+    },
+    id: {
+        nav:{
+            switchto: 'Ganti ke bahasa',
+            backtolanding: 'Kembali ke Halaman Utama',
+             langenglish: 'Inggris',
+            langindo: 'Bahasa Indonesia'
+        },
+        hero: {
+            vision: "Memberdayakan masyarakat Bali dalam pelestarian budaya Bali melalui upaya digitasi yang bertanggung jawab, berkelanjutan, inovatif, dan kolaboratif."
+        },
+        about:{
+            title: "Tentang", 
+            maintext: "Didirikan pada tahun 2024, BADHI merupakan upaya bersama antara akademisi Australia dan Indonesia yang meyakini bahwa teknologi digital saat ini dan masa depan memberikan peluang untuk meningkatkan pelestarian dan pemanfaatan warisan budaya Bali demi kebaikan sosial."
+        },
+        projects:{
+            title: "Warisan Budaya",
+            maintext: "Proyek-proyek BADHI bertujuan menciptakan nilai yang berkelanjutan bagi masyarakat Bali melalui pelestarian budaya. Pekerjaan kami mencakup warisan berwujud dan tak berwujud, dengan proyek-proyek saat ini berfokus pada pengarsipan digital pura dan pelestarian manuskrip lontar.",
+            temples:{
+                title:"Pura",
+                maintext: `Selama berabad-abad, pura-pura kuno Bali telah menghadapi proses kerusakan dan pembangunan kembali. Kami menciptakan solusi berkelanjutan untuk melestarikan pura-pura Bali bagi generasi mendatang.`
+            },
+            lontar:{
+                title:"Lontar",
+                maintext: 'Manuskrip lontar kuno Bali menyimpan teks-teks suci, sejarah, dan pengetahuan budaya selama berabad-abad. Pelestariannya sangat penting untuk melindungi warisan yang tak tergantikan ini bagi generasi mendatang. Kami mengeksplorasi teknik kecerdasan buatan untuk melindungi dan menafsirkan pengetahuan yang terkandung dalam manuskrip lontar tradisional Bali.'
+            }
+        },
+        contact:{
+            title: "Hubungi Kami",
+            follow: "Ikuti perkembangan kami di ",
+            link: "Beberapa tautan terkait:",
+            maintext: 'Jika Anda tertarik untuk mendukung, berpartisipasi, atau memiliki pertanyaa, silakan hubungi kami di badhi.program[at]gmail.com',
+            phdtext: 'Kami juga membuka peluang untuk riset <b>PhD</b> seputar topik teknologi dan budaya, kirim expression of interest <a href="https://supervisorconnect.it.monash.edu/projects/research/visualisation-technology-cultural-heritage-preservation" target="_blank">di sini</a>.'
+        },
+        team: {
+            maintext: 'BADHI terutama dipimpin oleh kolaborasi antara <a href="https://www.monash.edu/it/hcc/embodied-visualisation" target="_blank">Embodied Visualisation Group</a> (<b>EmVis</b>) di Monash University dan <a href="https://research.undiksha.ac.id/vvip-rg/" target="_blank">Virtual Vision, Image, and Pattern Group</a> (<b>VVIP</b>) di Universitas Pendidikan Ganesha. Dalam proyek Pengarsipan Digital Partisipatif Pura Bali, kami berkolaborasi dengan para ahli recordkeeping dari <a href="https://www.monash.edu/it/hcc/digital-transformation" target="_blank">Digital Transformation Group</a> (Monash University) dan para ahli action research dari <a href="https://www.monash.edu/it/hcc/action-lab" target="_blank">Action Lab</a> (Monash University).',
+            title: 'Tim',
+            academicstext: 'Akademisi yang berkolaborasi:',
+            ratext: 'Asisten peneliti',
+            studenttext: 'Mahasiswa peneliti',
+            supportedby: 'Upaya kami juga didukung oleh:',
+            specialthanks: 'Terima kasih khusus kepada Ketut Wiryadana yang telah membuat logo kami.'
+        }
+    }
+}
